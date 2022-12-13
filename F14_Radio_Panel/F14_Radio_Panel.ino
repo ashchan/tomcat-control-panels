cf60a37d04676faa63ffaefef2e57f33e97165f9
Jianming Chen <ashchan@gmail.com>
2022-12-13 21:14:33 +0900

    Connect some controls to joystick buttons

--- F14_Radio_Panel/F14_Radio_Panel.ino
+++ F14_Radio_Panel/F14_Radio_Panel.ino
@@ -5,12 +5,52 @@
 #include <Joystick.h>
 #include <NewEncoder.h>
 
+#define CHAN_SEL_CLK 0
+#define CHAN_SEL_DT 1
+#define TUNE 2
+#define LOAD 3
+#define VOL 4 // A6
+#define SQL 5
+#define BRT 6 // A7
+#define READ 7
+#define MODE_SEL 8 // A8
+#define FUNC_SEL 9 // A9
+#define MISC 10
 #define LED_DIO 14
 #define LED_CLK 15
 
+NewEncoder chanSelEncoder(CHAN_SEL_CLK, CHAN_SEL_DT, -20, 20, 0, FULL_PULSE);
 TM1637TinyDisplay6 led(LED_CLK, LED_DIO);
 Joystick_ joystick(0x07, JOYSTICK_TYPE_JOYSTICK, JOYSTICK_DEFAULT_BUTTON_COUNT,
   0, true, true, false, false, false, false, false, false, false, false, false);
+
+// PINs 0 - 10 state, PIN #1, #2 are not used as CHAN SEL Encoder has more friendly methods
+int lastControlState[11] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+void handleSimpleButton(int pin, int joystickButton) {
+  int state = digitalRead(pin);
+  if (state != lastControlState[pin]) {
+    joystick.setButton(joystickButton, state == LOW ? 1 : 0);
+    lastControlState[pin] = state;
+  }
+}
+
+// A0 - A3, 4 frequency toggle switches
+#define FREQ_SWITCH_PIN_1 A0
+#define FREQ_SWITCH_STATE_UP    1
+#define FREQ_SWITCH_STATE_OFF   2
+#define FREQ_SWITCH_STATE_DOWN  3
+int lastFreqSwitchState[4] = {FREQ_SWITCH_STATE_OFF, FREQ_SWITCH_STATE_OFF, FREQ_SWITCH_STATE_OFF, FREQ_SWITCH_STATE_OFF};
+
+int freqSwitchState(int value) {
+  if (value < 100) {
+    return FREQ_SWITCH_STATE_UP;
+  }
+  if (value < 900) {
+    return FREQ_SWITCH_STATE_DOWN;
+  }
+  return FREQ_SWITCH_STATE_OFF;
+}
 
 //-----------------------------------------------------------------------------
 // F-14B
@@ -100,16 +140,87 @@ DcsBios::IntegerBuffer uhfFreqBuffer(0x768e, 0x6000, 13, onF5UhfFreqChange);
 // Main loop
 void setup() {
   DcsBios::setup();
-  joystick.begin();
   Serial.begin(9600);
+
+  chanSelEncoder.begin();
+  pinMode(TUNE, INPUT_PULLUP);
+  pinMode(LOAD, INPUT_PULLUP);
+  pinMode(SQL, INPUT_PULLUP);
+  pinMode(READ, INPUT_PULLUP);
+  pinMode(MISC, INPUT_PULLUP);
 
   led.setBrightness(BRIGHT_3);
   led.clear();
   led.showString("-");
+
+  joystick.begin();
 }
 
 void loop() {
   DcsBios::loop();
+
+  if (chanSelEncoder.upClick()) {
+    joystick.pressButton(8);
+    delay(50);
+    joystick.releaseButton(8);
+  } else if (chanSelEncoder.downClick()) {
+    joystick.pressButton(9);
+    delay(50);
+    joystick.releaseButton(9);
+  }
+
+  handleSimpleButton(TUNE, 10);
+  handleSimpleButton(LOAD, 11);
+
+  if (lastControlState[VOL] != analogRead(VOL)) {
+  // TODO: VOL
+  }
+
+  if (lastControlState[SQL] != digitalRead(SQL)) {
+    if (digitalRead(SQL) == LOW) {
+      joystick.setButton(12, 1);
+      joystick.setButton(13, 0);
+    } else {
+      joystick.setButton(12, 0);
+      joystick.setButton(13, 1);
+    }
+    lastControlState[SQL] = digitalRead(SQL);
+  }
+
+  if (lastControlState[BRT] != analogRead(BRT)) {
+  // TODO: BRT
+  }
+
+  handleSimpleButton(READ, 14);
+
+  if (lastControlState[MODE_SEL] != analogRead(MODE_SEL)) {
+  // TODO: MODE_SEL
+  }
+
+  if (lastControlState[FUNC_SEL] != analogRead(FUNC_SEL)) {
+  // TODO: MODE_SEL
+  }
+
+  handleSimpleButton(MISC, 26);
+
+  // A0 - A3
+  for (int index = 0; index < 4; index++) {
+    int state = freqSwitchState(analogRead(FREQ_SWITCH_PIN_1 + index));
+    if (state != lastFreqSwitchState[index]) {
+      int joystickButton = index * 2;
+      int joystickValue = 1;
+      if (state == FREQ_SWITCH_STATE_OFF) {
+        joystickValue = 0;
+        if (lastFreqSwitchState[index] == FREQ_SWITCH_STATE_DOWN) {
+          joystickButton = index * 2 + 1;
+        }
+      } else if (state == FREQ_SWITCH_STATE_DOWN) {
+        joystickButton = index * 2 + 1;
+      }
+      joystick.setButton(joystickButton, joystickValue);
+      lastFreqSwitchState[index] = state;
+    }
+  }
 
   delay(40);
 }
