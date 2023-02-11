74a9bc03f400f2e9daae2f3fa479e303714a94ce
Jianming Chen <ashchan@gmail.com>
2023-02-11 13:23:29 +0900

    Update AFCS button handler

--- AFCS Panel/F14_AFCS_Panel/F14_AFCS_Panel.ino
+++ AFCS Panel/F14_AFCS_Panel/F14_AFCS_Panel.ino
@@ -8,9 +8,11 @@ using namespace ace_button;
 #define PIN_ROLL        0
 #define PIN_YAW         2
 #define PIN_VEC         3
-#define PIN_ALT         4
-#define PIN_HDG         5
-#define PIN_ENGAGE      6
+#define PIN_ACL         4
+#define PIN_ALT         5
+#define PIN_HDG         6
+#define PIN_GT          7
+#define PIN_ENGAGE      8
 
 #define PIN_LED_PITCH   9
 #define PIN_LED_ROLL    10
@@ -27,15 +29,24 @@ int hdg = 1;
 int engage = 0;
 
 AceButton vecButton(PIN_VEC);
+AceButton aclButton(PIN_ACL);
 AceButton hdgButton(PIN_HDG);
+AceButton gtButton(PIN_GT);
 AceButton engageButton(PIN_ENGAGE);
 
 void vecButtonPressed() {
   // PLT_AUTOPLT_VECTOR_CARRIER, 0..2
+  if (vec == 2) {
+    sendDcsBiosMessage("PLT_AUTOPLT_VECTOR_CARRIER", "1");
+  } else {
+    sendDcsBiosMessage("PLT_AUTOPLT_VECTOR_CARRIER", "2");
+  }
+}
+
+void aclButtonPressed() {
+  // PLT_AUTOPLT_VECTOR_CARRIER, 0..2
   if (vec == 0) {
     sendDcsBiosMessage("PLT_AUTOPLT_VECTOR_CARRIER", "1");
-  } else if (vec == 1) {
-    sendDcsBiosMessage("PLT_AUTOPLT_VECTOR_CARRIER", "2");
   } else {
     sendDcsBiosMessage("PLT_AUTOPLT_VECTOR_CARRIER", "0");
   }
@@ -43,10 +54,17 @@ void vecButtonPressed() {
 
 void hdgButtonPressed() {
   // PLT_AUTOPLT_HDG, 0..2
+  if (hdg == 2) {
+    sendDcsBiosMessage("PLT_AUTOPLT_HDG", "1");
+  } else {
+    sendDcsBiosMessage("PLT_AUTOPLT_HDG", "2");
+  }
+}
+
+void gtButtonPressed() {
+  // PLT_AUTOPLT_HDG, 0..2
   if (hdg == 0) {
     sendDcsBiosMessage("PLT_AUTOPLT_HDG", "1");
-  } else if (hdg == 1) {
-    sendDcsBiosMessage("PLT_AUTOPLT_HDG", "2");
   } else {
     sendDcsBiosMessage("PLT_AUTOPLT_HDG", "0");
   }
@@ -58,17 +76,25 @@ void engageButtonPressed() {
 }
 
 void handleEvent(AceButton* button, uint8_t eventType, uint8_t buttonState) {
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      if (button->getPin() == PIN_VEC) {
-        vecButtonPressed();
-      }
-      if (button->getPin() == PIN_HDG) {
-        hdgButtonPressed();
-      }
-      if (button->getPin() == PIN_ENGAGE) {
-        engageButtonPressed();
-      }
+  if (eventType != AceButton::kEventPressed) {
+    return;
+  }
+
+  switch (button->getPin()) {
+    case PIN_VEC:
+      vecButtonPressed();
+      break;
+    case PIN_ACL:
+      aclButtonPressed();
+      break;
+    case PIN_HDG:
+      hdgButtonPressed();
+      break;
+    case PIN_GT:
+      gtButtonPressed();
+      break;
+    case PIN_ENGAGE:
+      engageButtonPressed();
       break;
   }
 }
@@ -116,10 +142,14 @@ void setup() {
   pinMode(PIN_LED_GT, OUTPUT);
 
   pinMode(PIN_VEC, INPUT_PULLUP);
+  pinMode(PIN_ACL, INPUT_PULLUP);
   pinMode(PIN_HDG, INPUT_PULLUP);
+  pinMode(PIN_GT, INPUT_PULLUP);
   pinMode(PIN_ENGAGE, INPUT_PULLUP);
   vecButton.setEventHandler(handleEvent);
+  aclButton.setEventHandler(handleEvent);
   hdgButton.setEventHandler(handleEvent);
+  gtButton.setEventHandler(handleEvent);
   engageButton.setEventHandler(handleEvent);
 
   DcsBios::setup();
@@ -129,6 +159,8 @@ void loop() {
   DcsBios::loop();
 
   vecButton.check();
+  aclButton.check();
   hdgButton.check();
+  gtButton.check();
   engageButton.check();
 }
