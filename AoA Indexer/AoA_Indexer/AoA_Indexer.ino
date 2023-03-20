8bc9d0e36d0450a758c69e442d9a273632005b05
Jianming Chen <ashchan@gmail.com>
2023-03-20 18:21:07 +0900

    Change AoA indexer pin

--- AoA Indexer/AoA_Indexer/AoA_Indexer.ino
+++ AoA Indexer/AoA_Indexer/AoA_Indexer.ino
@@ -9,8 +9,8 @@ DcsBios::LED pltAoaFast(0x12d4, 0x0010, 4);
 
 DcsBios::LED pltHudLightWheels(0x12d6, 0x0008, 5);
 DcsBios::LED pltHudLightBrakes(0x12d6, 0x0010, 6);
-DcsBios::LED pltHudLightAclsap(0x12d6, 0x0020, 7);
-DcsBios::LED pltHudLightNws(0x12d6, 0x0040, 8);
+DcsBios::LED pltHudLightAclsap(0x12d6, 0x0020, 8);
+DcsBios::LED pltHudLightNws(0x12d6, 0x0040, 7);
 DcsBios::LED pltHudLightAutothr(0x12d6, 0x0080, 9);
 
 // F-16
